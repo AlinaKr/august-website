aaaad08b7312e01550380b617b549222ac07b2cc
J Nicholson <jenicholson.nc@gmail.com>
2018-08-28 17:28:16 +0200

    cleaned up

--- routes/index.js
+++ routes/index.js
@@ -6,18 +6,14 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 router.get('/nicole', (req, res, next) => {
-  res.render('nicole')
-})
-=======
-=======
+  res.render('nicole');
+});
+
 router.get('/ana', (req, res, next) => {
   res.render('ana');
->>>>>>> 6900225d64cc4da20b01ecdbae35b3bb05272a0c
-=======
+});
+
 router.get('/nhan', (req, res, next) => {
   res.render('nhan');
 });
@@ -25,7 +21,6 @@ router.get('/nhan', (req, res, next) => {
 router.get('/ana', (req, res, next) => {
   res.render('ana');
 });
->>>>>>> 2ef2b768654402991793b9acf2d249a822d36eec
 
 router.get('/maxence', (req, res, next) => {
   res.render('maxence');
@@ -35,40 +30,27 @@ router.get('/jessica', (req, res, next) => {
   res.render('jessica');
 });
 
-<<<<<<< HEAD
->>>>>>> d43901ca584910e05c526cce2e37a4e10afb81fa
-=======
 router.get('/flo', (req, res, next) => {
   res.render('flowantsicecream');
 });
 
->>>>>>> 0636b78da2c4c536cc7009ded1d4133b24c93776
 router.get('/jen', (req, res, next) => {
   res.render('jen');
 });
 router.get('/carla', (req, res, next) => {
   res.render('carla');
 });
-<<<<<<< HEAD
+
 router.get('/maxence', (req, res, next) => {
   res.render('maxence');
 });
-=======
 
 router.get('/anita-kash', (req, res, next) => {
   res.render('anita-kash');
 });
 
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> d43901ca584910e05c526cce2e37a4e10afb81fa
-=======
-module.exports = router;
->>>>>>> a5e97e41f3bbe58bcaaf326d99aa370bbf22e7c6
-=======
 router.get('/alina', (req, res, next) => {
   res.render('Alina');
 });
->>>>>>> 0636b78da2c4c536cc7009ded1d4133b24c93776
+
 module.exports = router;
->>>>>>> 01c7a7cc4e8a9d2c8898eb2f265ef759dc4aed2f
